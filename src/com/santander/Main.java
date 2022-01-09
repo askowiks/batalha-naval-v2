b2a944fe9e7c8d8cc4898bdb8d4c6130e2e6ff7f
askowiks <askowiks@gmail.com>
2022-01-09 13:30:45 -0300

    Finalizando Jogo

--- src/com/santander/Main.java
+++ src/com/santander/Main.java
@@ -6,15 +6,90 @@ public class Main {
 
     public static void main(String[] args) {
         int tamanhoTabuleiro = 5;
+        int qtdNavios = 5;
         int[][] tabuleiro = new int[tamanhoTabuleiro][tamanhoTabuleiro];
         String[][] campoDeBatalha = new String[tamanhoTabuleiro][tamanhoTabuleiro];
+
         exibirTabuleiro(tabuleiro);
-        posicionarNavio(tabuleiro);
+        posicionarNavio(tabuleiro, qtdNavios);
         iniciarCampoBatalha(campoDeBatalha);
-        atirar(campoDeBatalha, tabuleiro);
+        atirar(campoDeBatalha, tabuleiro, qtdNavios);
     }
 
+    public static void exibirTabuleiro(int[][] tabuleiro){
+
+        System.out.println(
+                "\n__<->__ TABULEIRO __<->__\n" +
+                        "  ║ 0 | 1 | 2 | 3 | 4 |\n" +
+                        "═══════════════════════"
+        );
+
+        for (int i = 0; i < tabuleiro.length; i++) {
+            System.out.print(i + " ║");
+
+            for (int j = 0; j < tabuleiro[i].length; j++) {
+
+                if (tabuleiro[i][j] == 0){
+                    System.out.print(" ~ |");
+                }else{ System.out.print("<->|");}
+
+            }System.out.print("\n");
+        }
+    }
+
+    public static void posicionarNavio(int[][] tabuleiro, int qtdNavios){
+        Scanner entrada = new Scanner(System.in);
+        int horizontal;
+        int vertical;
+        String posicaoLida;
+        int contNavio = 1;
+
+
+           do {
+                 try {
+                   System.out.println("\nEscolha a posição  do seu " + contNavio + "º navio (separado por \",\")");
+                   posicaoLida = entrada.nextLine(); //5,9
+                   var posicaoArray = posicaoLida.trim().split(",");
+                   horizontal = Integer.parseInt(posicaoArray[0].trim());
+                   vertical = Integer.parseInt(posicaoArray[1].trim());
+
+                   if (horizontal >= tabuleiro.length || vertical >= tabuleiro.length) {
+                       System.out.println("Posição inválida!");
+                       contNavio--;
+                   } else if (tabuleiro[horizontal][vertical] == 1) {
+                       System.out.println("Ops, já tem um navio aqui!");
+                       contNavio--;
+                   } else {
+                       tabuleiro[horizontal][vertical] = 1;
+                       contNavio++;
+                       System.out.println("Navio :" + contNavio +"/" + qtdNavios);
+                   }
+                   exibirTabuleiro(tabuleiro);
+                 }catch (Exception e){
+                   System.out.println("Posição inválida!");
+                }
+
+           }while (contNavio <= qtdNavios);
+   }
+
     public static void iniciarCampoBatalha (String[][] campoDeBatalha){
+
+        System.out.println(
+                ".\n \n \n \n \n \n \n \n \n \n " +
+                        "BEM VINDO A BATALHA NAVAL! \n"+
+                        "............,|)......|_\\\n" +
+                        "............/|)......|).\\\n" +
+                        ".........../.|)......|)..\\\n" +
+                        "........../..|)......|)...\\\n" +
+                        "........./...|)......|)....\\\n" +
+                        "......../....|).......|).....\\\n" +
+                        "......./...,-- ---_...|.......\\\n" +
+                        "....'\\==-,,;,,;,,;,,,,,,,-==;7~.~.~.~.~.~\n" +
+                        "....~.\\__....__...__.....__/~.~.~.~.~.~\n" +
+                        "...~.~.~.~.~.~.~.~.~.~.~.~.~r..?.\n" +
+                        "\n"
+        );
+
         System.out.println(
                 "\n------------------------------------------\n" +
                         "    CAMPO DE BATALHA\n"+
@@ -48,35 +123,16 @@ public class Main {
         }
     }
 
-    public static void exibirTabuleiro(int[][] tabuleiro){
-
-        System.out.println(
-             "\n__<->__ TABULEIRO __<->__\n" +
-                "  ║ 0 | 1 | 2 | 3 | 4 |\n" +
-                "═══════════════════════"
-        );
-
-        for (int i = 0; i < tabuleiro.length; i++) {
-            System.out.print(i + " ║");
-
-            for (int j = 0; j < tabuleiro[i].length; j++) {
-
-                if (tabuleiro[i][j] == 0){
-                    System.out.print(" ~ |");
-                }else{ System.out.print("<->|");}
-
-            }System.out.print("\n");
-        }
-    }
-
-    public static void atirar(String[][] campoDeBatalha, int[][] tabuleiro){
+    public static void atirar(String[][] campoDeBatalha, int[][] tabuleiro, int qtdNavios){
         Scanner entrada = new Scanner(System.in);
         int horizontal;
         int vertical;
         String posicaoLida;
+        int naviosAfundados = 0;
 
-        for (int contTiros = 1; contTiros <= 10; contTiros++){
-            System.out.println("Escolha a posição  do seu " + contTiros +"º tiro (separado por \",\")");
+        do {
+            try {
+            System.out.println("Escolha a posição  do seu tiro (separado por \",\")");
             posicaoLida = entrada.nextLine(); //5,9
             var posicaoArray = posicaoLida.trim().split(",");
             horizontal = Integer.parseInt(posicaoArray[0].trim());
@@ -84,36 +140,26 @@ public class Main {
 
             if (horizontal >= tabuleiro.length || vertical >= tabuleiro.length){
                 System.out.println("Posição inválida!");
-                contTiros--;}else if (tabuleiro[horizontal][vertical] == 1) {
+            }else if (tabuleiro[horizontal][vertical] == 1) {
+                if (campoDeBatalha[horizontal][vertical] == "X"){
+                    naviosAfundados--;
+                    System.out.println("Você já acertou esse navio!");
+                }
                 campoDeBatalha[horizontal][vertical] = "X";
-            }else{campoDeBatalha[horizontal][vertical] = "O";}
+                naviosAfundados++;
+                System.out.println("Você já atingiu " + naviosAfundados + ", faltam apenas " + (qtdNavios-naviosAfundados) + "!");
+            }else{campoDeBatalha[horizontal][vertical] = "O";
+                System.out.println("Ops, você errou!");
+            }
 
-           exibirCampoBatalha(campoDeBatalha);
+            exibirCampoBatalha(campoDeBatalha);
+            } catch (Exception e) {
+                System.out.println("Posição inválida, digite novamente");
+            }
+        }while (naviosAfundados < 5);
 
-        }
-    }
-
-    public static void posicionarNavio(int[][] tabuleiro){
-        Scanner entrada = new Scanner(System.in);
-        int horizontal;
-        int vertical;
-        String posicaoLida;
-
-        for (int contNavio = 1; contNavio <= 5; contNavio++){
-            System.out.println("\nEscolha a posição  do seu " + contNavio +"º navio (separado por \",\")");
-            posicaoLida = entrada.nextLine(); //5,9
-            var posicaoArray = posicaoLida.trim().split(",");
-            horizontal = Integer.parseInt(posicaoArray[0].trim());
-            vertical = Integer.parseInt(posicaoArray[1].trim());
-
-            if (horizontal >= tabuleiro.length || vertical >= tabuleiro.length){
-                System.out.println("Posição inválida!");
-                contNavio--;} else if (tabuleiro[horizontal][vertical] == 1){
-                System.out.println("Ops, já tem um navio aqui!");
-                contNavio--;
-            }else{
-            tabuleiro[horizontal][vertical] = 1;}
-            exibirTabuleiro(tabuleiro);
+        if (naviosAfundados == 5){
+            System.out.println("PARABÉNS! VOCÊ DERRUBOU TODOS OS NAVIOS!");
         }
     }
 
